a1f611b3202beabd395e6d1fdc065f8ee8154986
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-12-31 14:53:56 -0500

    Add Webpack 5 hook and specify webpack peerDep

---
+++ hooks.js
@@ -0,0 +1,19 @@
+const { SyncHook } = require('tapable');
+
+const JigsawHooksMap = new WeakMap();
+
+function getJigsawHooks(compilation) {
+    let hooks = JigsawHooksMap.get(compilation);
+
+    if (hooks === undefined) {
+        hooks = {
+            done: new SyncHook([]),
+        };
+
+        JigsawHooksMap.set(compilation, hooks);
+    }
+
+    return hooks;
+}
+
+module.exports = { getJigsawHooks };
